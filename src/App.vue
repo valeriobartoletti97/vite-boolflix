65b6de82d5bb7e89f1055f585fd9cf5fbd87d458
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-11-15 12:21:56 +0100

    scuffolding completed

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="fa-solid fa-circle">
+     
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import {store} from './data/store';
+  export default {
+    components:{
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
