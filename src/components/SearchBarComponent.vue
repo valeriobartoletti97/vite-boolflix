f3dc41468a1c2dd93a233ce221adafcc3f52619d
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-11-17 13:53:38 +0100

    searchbar format modified

--- src/components/SearchBarComponent.vue
+++ src/components/SearchBarComponent.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="d-flex flex-no-wrap">
-      <input type="text" name="searchbar" id="searchbar" v-model="searchTest" placeholder="Search.." @keyup.enter="filterData">
-      <button class="btn" @click="filterData">Search</button>
+  <div class="search-box">
+    <button class="btn-search" @click="filterData"><i class="fas fa-search"></i></button>
+    <input type="text" class="input-search" placeholder="Type to Search..." @keyup.enter="filterData" v-model="searchTest">
   </div>
 </template>
 
@@ -31,11 +31,64 @@ import { store } from '../data/store';
 </script>
 
 <style lang="scss" scoped>
-   input,button{
-            height: 70%;
-            margin: auto;
-        }
-        button{
-            color:white;
-        }
+@use '../assets/style/partials/variables.scss' as *;
+.search-box{
+  width: fit-content;
+  height: fit-content;
+  position: relative;
+}
+.input-search{
+  height: 50px;
+  width: 50px;
+  border-style: none;
+  padding: 10px;
+  font-size: 18px;
+  letter-spacing: 2px;
+  outline: none;
+  border-radius: 25px;
+  transition: all .5s ease-in-out;
+  background-color: $flame;
+  padding-right: 40px;
+  color:#fff;
+}
+.input-search::placeholder{
+  color:rgba(255,255,255,.5);
+  font-size: 18px;
+  letter-spacing: 2px;
+  font-weight: 100;
+}
+.btn-search{
+  width: 50px;
+  height: 50px;
+  border-style: none;
+  font-size: 20px;
+  font-weight: bold;
+  outline: none;
+  cursor: pointer;
+  border-radius: 50%;
+  position: absolute;
+  right: 0px;
+  color:#ffffff ;
+  background-color:transparent;
+  pointer-events: painted;  
+}
+.btn-search:focus ~ .input-search{
+  width: 300px;
+  border-radius: 0px;
+  background-color: transparent;
+  border-bottom:1px solid rgba(255,255,255,.5);
+  transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+}
+.input-search:focus{
+  width: 300px;
+  border-radius: 0px;
+  background-color: transparent;
+  border-bottom:1px solid rgba(255,255,255,.5);
+  transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+}
 </style>
+
+<!--   <div class="d-flex flex-no-wrap">
+      <input type="text" name="searchbar" id="searchbar" placeholder="Search.." @keyup.enter="filterData" v-model="searchTest">
+      <button class="btn" @click="filterData">Search</button>
+  </div> -->
